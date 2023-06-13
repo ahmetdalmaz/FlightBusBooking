1a02af06663f64c17ccb9c967df9faacc1e2a81a
ahmett.dalmaz@gmail.com <ahmett.dalmaz@gmail.com>
2023-06-13 11:31:55 +0300

    begin adding user controller and auto mapper

---
+++ FlightBusBooking.WebUI/MappingProfiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FlightBusBooking.Entities.Concrete;
+using FlightBusBooking.WebUI.Areas.Admin.Models.UserModels;
+
+namespace FlightBusBooking.WebUI.MappingProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserModel,AppUser>().ReverseMap();
+        }
+    }
+}
